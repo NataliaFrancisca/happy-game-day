67fd806fa534987370bcea7f3403eb70f894c2c9
NataliaFrancisca <nathifrancisca@gmail.com>
2024-01-25 23:39:13 -0300

    refactor: use arrow function at the line 9

--- app/view-matches/page.tsx
+++ app/view-matches/page.tsx
@@ -6,18 +6,16 @@ export default function PageViewMatches(){
 
     const { matches, onUpdateCurrentMatches } = useHandleMatches();
 
-    const shouldUpdateTheMatches = (number:number) => {
-        return number % 2 == 1;
-    }
+    const shouldUpdateTheMatches = (number:number) => number % 2 == 1;
 
     return(
-        <section className="p-5 bg-white w-11/12 min-h-screen h-auto m-auto">
+        <section className="p-5 bg-transparent w-11/12 min-h-screen h-auto m-auto">
             {matches != undefined && 
                 matches.map((match, key) => (
                     <section key={key+1} className="mb-10">
-                        <p>{key+1}</p>
                         <Match data={match.round_one} updateState={onUpdateCurrentMatches} shouldUpdateValues={shouldUpdateTheMatches(key+1)} />
                         <Match data={match.round_two} updateState={onUpdateCurrentMatches} shouldUpdateValues={shouldUpdateTheMatches(key+1)} />
+                        {(shouldUpdateTheMatches(key+1) && key+1 > 1) && <div className="h-10 bg-slate-300 w-full my-10"></div>}
                     </section>
                 ))
             }
