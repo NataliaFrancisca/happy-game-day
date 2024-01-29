92a1f9c8bf453b72bd7bff57d98878894f725fa7
NataliaFrancisca <nathifrancisca@gmail.com>
2024-01-29 19:46:43 -0300

    feat: add a new statement to check if the match should update the matches

--- app/view-matches/page.tsx
+++ app/view-matches/page.tsx
@@ -4,18 +4,24 @@ import { useHandleMatches } from "../hooks/useHandleMatches"
 
 export default function PageViewMatches(){
 
-    const { matches, onUpdateCurrentMatches } = useHandleMatches();
+    const { matches, onUpdateCurrentMatches, appOption } = useHandleMatches();
 
-    const shouldUpdateTheMatches = (number:number) => number % 2 == 1;
+    const shouldUpdateTheMatches = (number:number) => {
+        if(appOption === 'OP-WINNER-LOOSER'){
+            return true;
+        }else{
+            return number % 2 == 1;
+        }
+    }
 
     return(
         <section className="p-5 bg-transparent w-11/12 min-h-screen h-auto m-auto">
             {matches != undefined && 
                 matches.map((match, key) => (
                     <section key={key+1} className="mb-10">
-                        <Match data={match.round_one} updateState={onUpdateCurrentMatches} shouldUpdateValues={shouldUpdateTheMatches(key+1)} />
-                        <Match data={match.round_two} updateState={onUpdateCurrentMatches} shouldUpdateValues={shouldUpdateTheMatches(key+1)} />
-                        {(shouldUpdateTheMatches(key+1) && key+1 > 1) && <div className="h-10 bg-slate-300 w-full my-10"></div>}
+                        <Match data={match.roundOne} updateState={onUpdateCurrentMatches} shouldUpdateValues={shouldUpdateTheMatches(key+1)} />
+                        <Match data={match.roundTwo} updateState={onUpdateCurrentMatches} shouldUpdateValues={shouldUpdateTheMatches(key+1)} />
+                        {/* {(shouldUpdateTheMatches(key+1) && key+1 > 1) && <div className="h-10 bg-slate-300 w-full my-10"></div>} */}
                     </section>
                 ))
             }
