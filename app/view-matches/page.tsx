333926c1b7876fac97716043d08c771ec01c5de6
NataliaFrancisca <nathifrancisca@gmail.com>
2024-01-29 21:34:38 -0300

    feat: add a title for the page and decrease the padding

--- app/view-matches/page.tsx
+++ app/view-matches/page.tsx
@@ -15,10 +15,11 @@ export default function PageViewMatches(){
     }
 
     return(
-        <section className="p-5 bg-transparent w-11/12 min-h-screen h-auto m-auto">
+        <section className="py-5 px-2 bg-transparent w-11/12 min-h-screen h-auto m-auto">
+            <h2 className="font-fasterone text-2xl mb-6">MATCHES:</h2>
             {matches != undefined && 
                 matches.map((match, key) => (
-                    <section key={key+1} className="mb-10">
+                    <section key={key+1} className="mb-10 rounded-md border-4 border-solid border-black">
                         <Match data={match.roundOne} updateState={onUpdateCurrentMatches} shouldUpdateValues={shouldUpdateTheMatches(key+1)} />
                         <Match data={match.roundTwo} updateState={onUpdateCurrentMatches} shouldUpdateValues={shouldUpdateTheMatches(key+1)} />
                         {/* {(shouldUpdateTheMatches(key+1) && key+1 > 1) && <div className="h-10 bg-slate-300 w-full my-10"></div>} */}
