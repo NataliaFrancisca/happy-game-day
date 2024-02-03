d274deb43baf2d2f7c2c2990584b14d978ae8492
NataliaFrancisca <nathifrancisca@gmail.com>
2024-02-03 20:43:14 -0300

    feat: add new interfaces

--- app/ts/interface.ts
+++ app/ts/interface.ts
@@ -1,6 +1,18 @@
-import { TMatch } from "./types";
+import { Dispatch } from "react";
+import { TTeam, TTeamStatus } from "./types";
 
 export interface IButton{
-    data: TMatch,
+    data: TTeam,
     onHandleButton: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export interface IMatch{
+    teams: Array<TTeam>,
+    onUpdateMatchesState: (teams: TTeamStatus) => void,
+    index: number
+}
+
+export interface IFieldset{
+    labelMessage: string,
+    onChangeInput: Dispatch<string>
 }
